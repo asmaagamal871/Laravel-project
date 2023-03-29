f905e36f5a0c2690ba776da7280ad7d90567a9a3
mayar <mayarhamed15@gmail.com>
2023-03-29 06:43:49 +0200

    empty models

--- database/migrations/2023_03_28_233942_create_prescriptions_table.php
+++ database/migrations/2023_03_28_233942_create_prescriptions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order_prescription', function (Blueprint $table) {
+        Schema::create('prescriptions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->unsignedBigInteger('order_id');
